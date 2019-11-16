0dc042caf7faaa13008d6318d60e559d6db84c4d
Jardel Osorio Duarte <jardelduarte594@gmail.com>
2019-11-16 03:18:30 -0300

    add prints

--- models/connect.js
+++ models/connect.js
@@ -5,13 +5,14 @@ const Sequelize = require('sequelize')
 		host: '127.0.0.1',
 		dialect: 'mysql'
 	})
+
 	module.exports = {
 		Sequelize: Sequelize,
 		sequelize: sequelize
 	};
-/*
+
 sequelize.authenticate().then(function(){
 	console.log("conectado com sucesso!")
 }).catch(function(erro){
 	catchonsole.log("Falha ao se conectar! " + erro)
-})*/ 
+}) 
